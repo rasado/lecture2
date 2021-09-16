eb857fa1763cf0b6bea7f51b10255a36d9d6ec15
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-09-16 14:08:00 +0200

    phil mick

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-=======
+
         System.out.println("Tiger\nda\ngoat");
->>>>>>> tigerwoods
+
+        System.out.println("phil mickelson");
+
     }
 }
