f0fe979165efff814ccf1f73e91cb728c0d22c40
Rasmus Adolfsson <rasmus.adolfsson@student.kyh.se>
2021-09-16 14:10:47 +0200

    majors tiger

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tiger\nda\ngoat");
+
+        System.out.println("flest majors han löser det");
     }
 }
